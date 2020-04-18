4062901390403f85cc56e40bcf14741d4c758824
tomaszlach <tomaszlach86@gmail.com>
2020-04-18 17:20:51 +0200

    added some scripts

--- js/javascript.js
+++ js/javascript.js
@@ -5,11 +5,18 @@
 
 var $arrow = $("#arrow a");
 
+var $arrowDown = $("#arrowDown a");
 
 
 $("#arrow").on("click", function () {
     $("body, html").animate({
         scrollTop: $("main").offset().top
+    }, 1000);
+});
+
+$("#arrowDown").on("click", function () {
+    $("body, html").animate({
+        scrollTop: -$("main").offset().top
     }, 1000);
 });
 
@@ -23,6 +30,22 @@ const handleClick = () => {
 }
 
 hamburger.addEventListener('click', handleClick);
+
+
+// $("#formoid").submit(function(event) {
+//     /* stop form from submitting normally */
+//     event.preventDefault();
+//     /* get the action attribute from the <form action=""> element */
+//     var $form = $( this ), url = $form.attr( 'action' );
+//
+//     /* Send the data using post with element id name and name2*/
+//     var posting = $.post( url, { name: $('#name').val(), name2: $('#name2').val() } );
+//
+//     /* Alerts the results */
+//     posting.done(function( data ) {
+//         alert('success');
+//     });
+// });
 
 
 
